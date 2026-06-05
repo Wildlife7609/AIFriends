0bef1d85d7be8694eda7ca53c9f6167de400573a
Jing Li <dlijing4869@gmail.com>
2026-06-04 22:09:08 -0700

    added router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    { path: '/', name: 'homepage', component: () => import('@/views/homepage/HomepageIndex.vue') },
+    { path: '/friend', name: 'friend', component: () => import('@/views/friend/FriendIndex.vue') },
+    { path: '/create', name: 'create', component: () => import('@/views/create/CreateIndex.vue') },
+    { path: '/profile', name: 'profile', component: () => import('@/views/profile/ProfileIndex.vue') },
+    { path: '/login', name: 'login', component: () => import('@/views/user/account/LoginIndex.vue') },
+    { path: '/register', name: 'register', component: () => import('@/views/user/account/RegisterIndex.vue') },
+    { path: '/space/:user_id', name: 'user-space', component: () => import('@/views/user/space/SpaceIndex.vue') },
+    { path: '/404', name: '404', component: () => import('@/views/error/NotFoundIndex.vue') },
+    { path: '/:pathMatch(.*)*', redirect: '/404' },
   ],
 })
 
