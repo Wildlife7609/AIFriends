6f7a531afa1344139e2d8f1ffce4747c26ff2a3d
Jing Li <dlijing4869@gmail.com>
2026-06-06 13:11:52 -0700

    define database and achiveve it

--- backend/web/models/user.py
+++ backend/web/models/user.py
@@ -1,15 +1,24 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.timezone import now, localtime
+import uuid
+
+
+def photo_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4().hex[:16]}.{ext}'
+    return f'user/photos/{instance.user.id}_{filename}'
 
 
 class UserProfile(models.Model):
+    # Links to Django's built-in User (already has username, email, password)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='profile_pics/', default='photos/default.png', blank=True, null=True)
-    username = models.CharField(max_length=128, unique=True)
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(blank=True, null=True)
-    bio = models.TextField(blank=True, null=True)
-    address = models.CharField(max_length=256, blank=True, null=True)
-    phone = models.CharField(max_length=32, blank=True, null=True)
-    
-    
+
+    # Extra profile fields not in User
+    photo = models.ImageField(default='user/photos/default.png', upload_to=photo_upload_path)
+    profile = models.TextField(default='thanks for interesting in my profile!', max_length=500)
+    create_time = models.DateTimeField(default=now)
+    update_time = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f'{self.user.username} - {localtime(self.create_time).strftime("%Y-%m-%d %H:%M:%S")}'
