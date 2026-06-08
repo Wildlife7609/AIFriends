df6c82a7ad189335734b0d2adbbf8800a45d52d9
Jing Li <dlijing4869@gmail.com>
2026-06-08 00:46:29 -0700

    fix API router

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -6,9 +6,9 @@ from web.views.user.account.logout import LogoutView
 from web.views.user.account.refresh_token import RefreshTokenView
 
 urlpatterns = [
-    path('api/user/login/', LoginView.as_view(), name='user_login'),
-    path('api/user/register/', RegisterView.as_view(), name='user_register'),
-    path('api/user/logout/', LogoutView.as_view(), name='user_logout'),
-    path('api/user/token/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
+    path('api/user/account/login/', LoginView.as_view(), name='user_account_login'),
+    path('api/user/account/register/', RegisterView.as_view(), name='user_account_register'),
+    path('api/user/account/logout/', LogoutView.as_view(), name='user_account_logout'),
+    path('api/user/account/refresh_token/', RefreshTokenView.as_view(), name='user_account_token_refresh'),
     path('', index, name='index'),
 ]
