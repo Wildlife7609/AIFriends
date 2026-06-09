5b82d017356b4ba2d6266e3a0f58e0489ad4c308
Jing Li <dlijing4869@gmail.com>
2026-06-08 22:27:06 -0700

    router beforeEach set success

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,16 +3,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'homepage', component: () => import('@/views/homepage/HomepageIndex.vue') },
-    { path: '/friend', name: 'friend', component: () => import('@/views/friend/FriendIndex.vue') },
-    { path: '/create', name: 'create', component: () => import('@/views/create/CreateIndex.vue') },
-    { path: '/profile', name: 'profile', component: () => import('@/views/profile/ProfileIndex.vue') },
-    { path: '/login', name: 'login', component: () => import('@/views/user/account/LoginIndex.vue') },
-    { path: '/register', name: 'register', component: () => import('@/views/user/account/RegisterIndex.vue') },
-    { path: '/space/:user_id', name: 'user-space', component: () => import('@/views/user/space/SpaceIndex.vue') },
-    { path: '/404', name: '404', component: () => import('@/views/error/NotFoundIndex.vue') },
-    { path: '/:pathMatch(.*)*', redirect: '/404' },
+    { path: '/', name: 'homepage', component: () => import('@/views/homepage/HomepageIndex.vue'), meta: { requiresAuth: false } },
+    { path: '/friend', name: 'friend', component: () => import('@/views/friend/FriendIndex.vue'), meta: { requiresAuth: true } },
+    { path: '/create', name: 'create', component: () => import('@/views/create/CreateIndex.vue'), meta: { requiresAuth: true } },
+    { path: '/profile', name: 'profile', component: () => import('@/views/profile/ProfileIndex.vue'), meta: { requiresAuth: true } },
+    { path: '/login', name: 'login', component: () => import('@/views/user/account/LoginIndex.vue'), meta: { requiresAuth: false } },
+    { path: '/register', name: 'register', component: () => import('@/views/user/account/RegisterIndex.vue'), meta: { requiresAuth: false } },
+    { path: '/space/:user_id', name: 'user-space', component: () => import('@/views/user/space/SpaceIndex.vue'), meta: { requiresAuth: true } },
+    { path: '/404', name: '404', component: () => import('@/views/error/NotFoundIndex.vue'), meta: { requiresAuth: false } },
+    { path: '/:pathMatch(.*)*', redirect: '/404', meta: { requiresAuth: false } },
   ],
 })
 
-export default router
+import { useUserStore } from '@/stores/user'
+
+router.beforeEach((to, from, next) => {
+  const user = useUserStore()
+  if (to.meta.requiresAuth && !user.isLogin()) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
+});
+
+export default router;
